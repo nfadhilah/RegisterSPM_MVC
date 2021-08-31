846383f413ae60669689f5bb5da0c9a5935bb4ed
mono <halilintar@gmail.com>
2021-08-31 13:33:53 +0800

    Add SideBarNavMenu View Component

---
+++ RegisterSPM/ViewComponents/SidebarNavMenuViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RegisterSPM.ViewComponents
+{
+    public class SidebarNavMenuViewComponent : ViewComponent
+    {
+      public IViewComponentResult Invoke()
+      {
+        return View();
+      }
+    }
+}
